0eb54014607a11627fa987a5826db96cf68b4a60
Jackson <jacksonhmteixeira@gmail.com>
2021-03-22 18:18:04 -0300

    CREATE - Adicionando Categoria e lista de Categorias / Atualizando tela de produto

---
+++ src/app/domain/categoria/categorias.ts
@@ -0,0 +1,51 @@
+import { Categoria } from './categoria';
+
+export const CATEGORIA: Categoria[] = [
+    {
+        id: 1,
+        categoria: 'e-book'
+    },
+    {
+        id: 2,
+        categoria: 'Curso Online'
+    },
+    {
+        id: 3,
+        categoria: 'Serviços de Consultoria'
+    },
+    {
+        id: 4,
+        categoria: 'Evento Online'
+    },
+    {
+        id: 5,
+        categoria: 'Assinatura'
+    },
+    {
+        id: 6,
+        categoria: 'Código Fonte'
+    },
+    {
+        id: 7,
+        categoria: 'Produto Físico'
+    },
+    {
+        id: 8,
+        categoria: 'Áudio / Música'
+    }
+    ,
+    {
+        id: 9,
+        categoria: 'Filme'
+    }
+    ,
+    {
+        id: 10,
+        categoria: 'Ingressos para eventos presenciais'
+    }
+    ,
+    {
+        id: 11,
+        categoria: 'Arquivos para Baixar'
+    }
+]
