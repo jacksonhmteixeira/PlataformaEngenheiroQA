2c47bc1576fdfc8dccc7e8cc18fe8e2dc2c2f922
Jackson <jacksonhmteixeira@gmail.com>
2021-04-08 09:51:39 -0300

    UPDATE - Atualizando datas do gráfico

--- src/app/domain/painel/grafico-trinta-dias/grafico-trinta-dias.component.ts
+++ src/app/domain/painel/grafico-trinta-dias/grafico-trinta-dias.component.ts
@@ -6,19 +6,38 @@ import { Color, Label } from "ng2-charts";
     selector: 'app-grafico-trinta-dias',
     templateUrl: './grafico-trinta-dias.component.html'
 })
-export class GraficoTrintaDiasComponent {
+export class GraficoTrintaDiasComponent implements OnInit {
+    
+    ultimosSeteDias = [];
+    myDate = new Date();
+
+    adicionando(diasArray){
+        for(let i = 0; i<7; i++){
+            diasArray.push((this.myDate.getDate()-i) + " / " + (this.myDate.getMonth() + 1))
+        }
+        alert(diasArray);
+        return diasArray;
+    }
+
+    constructor() { }
+
+    ngOnInit() { 
+        //this.ultimosSeteDias = this.identificandoUltimosSeteDias(this.ultimosSeteDias);
+        this.ultimosSeteDias = this.adicionando(this.ultimosSeteDias);
+    }
+    
     public lineChartData: ChartDataSets[] = [
         { data: [0, 0, 40.90, 70, 60, 100, 80], label: "Últimos 7 Dias" }
     ];
 
     public lineChartLabels: Label[] = [
-        "19/10",
-        "20/10",
-        "21/10",
-        "22/10",
-        "23/10",
-        "24/10",
-        "25/10"
+        this.ultimosSeteDias[0],
+        this.ultimosSeteDias[1],
+        this.ultimosSeteDias[2],
+        this.ultimosSeteDias[3],
+        this.ultimosSeteDias[4],
+        this.ultimosSeteDias[5],
+        this.ultimosSeteDias[6]
     ];
 
     public lineChartOptions: ChartOptions = {
@@ -32,11 +51,4 @@ export class GraficoTrintaDiasComponent {
     public lineChartLegend = true;
     public lineChartType = "line";
     public lineChartPlugins = [];
-
-    myDate = new Date();
-
-    datas: string[];
-    mostrarData() {
-        alert(this.myDate.getDate() + " - " + (this.myDate.getMonth() + 1) + " - " + this.myDate.getFullYear());
-    }
 }
